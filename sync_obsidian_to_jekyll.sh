bb829e1dcde0f76f9454df851fa8db963ee18889
Declan Bourke <declanbourke@TE5ED.WPA.Dal.Ca>
2024-09-25 15:58:20 -0300

    Your commit message describing the changes

--- sync_obsidian_to_jekyll.sh
+++ sync_obsidian_to_jekyll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -R "/Users/declanbourke/Library/Mobile Documents/iCloud~md~obsidian/Documents/Student Vault/"*.md ~/declanbourke.github.io/_posts/
+cp -r ~/Documents/ObsidianVault/*.md ~/declanbourke.github.io/_posts/
 cd ~/declanbourke.github.io
 git add _posts
 git commit -m "Sync markdown files from Obsidian"
